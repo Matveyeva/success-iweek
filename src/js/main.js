97adf83a8e55dd30505c85f2f189c976a17d3cd7
Lareta064 <lareta064@gmail.com>
2022-11-04 19:32:11 +0300

    script-for-menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (){
 	
 	const toggleMenu = document.querySelector('#toggle-menu');
-	
+	const mobMenu = document.querySelector('#header-mobile-menu');
 	toggleMenu.addEventListener('click', function(){
 		if(toggleMenu.classList.contains('active')){
 			this.classList.remove('active');
-			console.log(toggleMenu.classList);
+			mobMenu.style.maxHeight = 0+'px';
 		}else{
 			this.classList.add('active');
 			console.log(toggleMenu.classList);
+			mobMenu.style.maxHeight = mobMenu.scrollHeight+'px';
 		}
 		
 	});
