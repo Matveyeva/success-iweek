51d1d94eae9df18f8a7030de9c6acdc2186ae530
Lareta064 <lareta064@gmail.com>
2022-11-04 12:29:44 +0300

    скрипт-гамбургер

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (){
-
+	
+	const toggleMenu = document.querySelector('#toggle-menu');
+	
+	toggleMenu.addEventListener('click', function(){
+		if(toggleMenu.classList.contains('active')){
+			this.classList.remove('active');
+			console.log(toggleMenu.classList);
+		}else{
+			this.classList.add('active');
+			console.log(toggleMenu.classList);
+		}
+		
+	});
 });
