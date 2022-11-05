f28be6fc1886356913197c9b4af765ba62f1282e
Lareta064 <lareta064@gmail.com>
2022-11-05 12:54:34 +0300

    doc-crd-left

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,29 @@ document.addEventListener("DOMContentLoaded", function (){
 	
 	const toggleMenu = document.querySelector('#toggle-menu');
 	const mobMenu = document.querySelector('#header-mobile-menu');
-	toggleMenu.addEventListener('click', function(){
-		if(toggleMenu.classList.contains('active')){
-			this.classList.remove('active');
-			mobMenu.style.maxHeight = 0+'px';
-		}else{
-			this.classList.add('active');
-			console.log(toggleMenu.classList);
-			mobMenu.style.maxHeight = mobMenu.scrollHeight+'px';
-		}
-		
-	});
+	if(toggleMenu){
+		toggleMenu.addEventListener('click', function(){
+			if(toggleMenu.classList.contains('active')){
+				this.classList.remove('active');
+				mobMenu.style.maxHeight = 0+'px';
+			}else{
+				this.classList.add('active');
+				console.log(toggleMenu.classList);
+				mobMenu.style.maxHeight = mobMenu.scrollHeight+'px';
+			}
+			
+		});
+	}
+	/*=========RATING BLOCK =======*/
+	const ratingBlock = document.querySelectorAll('[data-rating]');
+	if(ratingBlock){
+		ratingBlock.forEach( function(item, index) {
+			const itemActive = item.querySelector('[data-count]');
+			const itemActiveCount = itemActive.querySelectorAll('i');
+			const itemActiveLength = itemActiveCount.length
+			const itemActiveVal = itemActive.getAttribute('data-count');
+			const activeBlockWidth = +(((itemActiveVal / itemActiveLength) * 100)+0.5) ;//0.5 погрешность нп расстояние между звездами
+			itemActive.style.width =`${activeBlockWidth}%`;
+		});
+	}
 });
