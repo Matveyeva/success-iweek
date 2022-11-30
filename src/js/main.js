eadcd217f7a0b175b36df960e5f3e762cb8f2657
Lareta064 <lareta064@gmail.com>
2022-11-30 10:12:44 +0300

    fix-modal

--- src/js/main.js
+++ src/js/main.js
@@ -38,6 +38,8 @@ document.addEventListener("DOMContentLoaded", function (){
 		if(modalHiddenXl.length > 0){		
 			if(this.innerWidth > 1199){
 				bodyEl.classList.remove('noscroll');
+				
+				
 				for(let item of  modalHiddenXl ){
 					item.classList.remove('visible');
 				}
@@ -54,9 +56,11 @@ document.addEventListener("DOMContentLoaded", function (){
 		}
 	});
 	/*========= =============== RATING BLOCK ===============*/
-	const ratingBlock = document.querySelectorAll('[data-rating]');
+	const ratingBlock = document.querySelectorAll('.rating-block');
 	if(ratingBlock){
+		
 		ratingBlock.forEach( function(item, index) {
+			
 			const itemActive = item.querySelector('[data-count]');
 			const itemActiveCount = itemActive.querySelectorAll('i');
 			const itemActiveLength = itemActiveCount.length
@@ -129,14 +133,16 @@ document.addEventListener("DOMContentLoaded", function (){
     const modalFramesOpen = document.querySelectorAll('[frame-btn]');
     const modalFrames = document.querySelectorAll('[frame-modal]');
     if( modalFrames.length > 0){
-      
       const modalFramesClose = document.querySelectorAll('[frame-close]');
+
       for(let item of modalFramesOpen){
         item.addEventListener('click', function(e){
           for(let item of  modalFrames){
             item.classList.remove('visible');
             
             bodyEl.classList.remove('noscroll');
+			
+			
           }
           e.preventDefault();
           const itemAttr = item.getAttribute('frame-btn');
@@ -156,14 +162,18 @@ document.addEventListener("DOMContentLoaded", function (){
           e.preventDefault();
           item.closest('[frame-modal]').classList.remove('visible');
           bodyEl.classList.remove('noscroll');
+		  
+		  
         });
       }
  	/*=============== закрыть модалки по клику вне ===============*/
 	for(let frame of modalFrames){
 		frame.addEventListener('click', function(e){
-			if(e.target === e.currentTarget)
+			if(e.target === e.currentTarget){
 				this.classList.remove(`visible`)
 				bodyEl.classList.remove('noscroll');
+				
+			}
 		});
 	}
 
